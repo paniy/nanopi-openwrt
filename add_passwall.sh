3cf07a518f1d4149406fc28f24a5d3f085e36550
潘羿 <33997505+paniy@users.noreply.github.com>
2021-02-14 23:57:45 +0800

    Update

--- add_passwall.sh
+++ add_passwall.sh
@@ -11,4 +11,8 @@ rm -rf package/lean/luci-app-kodexplorer
 rm -rf package/lean/luci-app-pppoe-relay
 rm -rf package/lean/luci-app-pptp-server
 rm -rf package/lean/luci-app-v2ray-server
+rm -rf package/lean/luci-app-passwall/ && svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
+sed -i 's,default n,default y,g' package/lean/luci-app-passwall/Makefile
+sed -i '/V2ray:v2ray/d' package/lean/luci-app-passwall/Makefile
+sed -i '/plugin:v2ray/d' package/lean/luci-app-passwall/Makefile
 ./scripts/feeds install -a -p lienol
